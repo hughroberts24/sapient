f1c7359d11a1a87c2135d82d0fd9b45e3ea72bc5
Noah Bernstein <noahbernstein@berkeley.edu>
2024-12-30 15:55:46 -0800

    beginning of metadata

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
@@ -9,6 +10,16 @@ import Work from './pages/Work';
 function App() {
   return (
     <Router>
+      <Helmet>
+        <title>Sapient Development</title>
+        <meta name="description" content="Sapient Development" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="Sapient Development" />
+        <meta property="og:description" content="Sapient Development" />
+        <meta property="og:image" content="/og-image.jpg" />
+        <meta property="og:url" content="https://yourwebsite.com" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Helmet>
       <div className="bg-black text-white min-h-screen">
         <Navigation />
         <Routes>
